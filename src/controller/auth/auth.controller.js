34bdabb640919dad18ef3665e627ae045e75398d
Manh <manhselu@gmail.com>
2026-05-16 15:23:49 +0700

    feat: register api logic

--- src/controller/auth/auth.controller.js
+++ src/controller/auth/auth.controller.js
@@ -1,13 +1,15 @@
 
-const { loginSchema } = require("./../../validation/auth.validation")
-const authService = require("./../../service/auth/auth.service")
+const { loginSchema, registerSchema } = require("./../../validation/auth.validation")
+const authService = require("./../../service/auth/auth.service");
+const { da } = require("zod/v4/locales");
+
 module.exports.login = async (req, res) => {
     try {
         const { phone, password } = req.body;
         const validation = loginSchema.safeParse({ phone, password });
         const result = await authService.login(phone, password);
         return res.status(200).json({
-            message: "Login success",
+            message: "Đăng nhập thành công",
             data: result
         });
     } catch (error) {
@@ -16,3 +18,24 @@ module.exports.login = async (req, res) => {
         });
     }
 };
+module.exports.register = async (req,res) => {
+    try {
+        const {fullName, phone, password, confirmPassword} = req.body;
+        const validation = registerSchema.safeParse({fullName, phone, password});
+         if (!validation.success) {
+            return res.status(400).json({
+                message: validation.error.issues [0].message
+            });
+        }
+        const result = await authService.register(fullName, phone, password, confirmPassword)
+        return res.status(200).json({
+            message: "Đăng kí thành công",
+            data: result
+        });
+    } catch (error) {
+        return res.status(error.status || 500).json({
+            message: error.message || "Internal server error"
+        });
+    }
+};
+
--- src/router/auth.routes.js
+++ src/router/auth.routes.js
@@ -5,5 +5,5 @@ const router = express.Router();
 const authController = require("../controller/auth/auth.controller");
 
 router.post("/login", authController.login);
-
+router.post("/register",authController.register);
 module.exports = router;
