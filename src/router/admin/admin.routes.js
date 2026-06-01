9b2782d962b7352ede9e9e95e7a22d44b1eff9a5
Trần Lương Bình <137519977+binhbinh214@users.noreply.github.com>
2026-06-01 22:51:20 +0700

    feat: add View/Create/Update Warranty policies API

--- src/router/admin/admin.routes.js
+++ src/router/admin/admin.routes.js
@@ -5,22 +5,29 @@ const controller = require("../../controller/admin/serviceCombos.controller");
 const serviceCatalogController = require("../../controller/admin/serviceCatalog.controller.");
 const staffController = require("../../controller/admin/manageStaff.controller");
 const controllerServiceBays = require("../../controller/admin/serviceBays.controller");
+const warrantyController = require("../../controller/admin/warrantyPolicies.controller");
 
-router.get("/role", staffController.getRoles)
+router.get("/role", staffController.getRoles);
 router.get("/staff", staffController.getStaffList);
 router.post("/staff", staffController.createStaff);
 router.put("/staff/:userId", staffController.updateStaff);
 
-router.get("/service-categories", serviceCatalogController.getServiceCategories);
+router.get(
+  "/service-categories",
+  serviceCatalogController.getServiceCategories
+);
 router.post("/service-catalog", serviceCatalogController.createServiceCatalog);
 router.get("/service-catalog", serviceCatalogController.getServiceCatalog);
-router.patch("/service-catalog/:id", serviceCatalogController.updateServiceCatalog);
+router.patch(
+  "/service-catalog/:id",
+  serviceCatalogController.updateServiceCatalog
+);
 
 router.get("/pricing-rules", pricingRulesController.getAllPricingRules);
 router.post("/pricing-rules", pricingRulesController.createPricingRules);
-router.get('/pricing-rules/:id', pricingRulesController.getPricingRuleById);
-router.put('/pricing-rules/:id', pricingRulesController.updatePricingRule);
-router.delete('/pricing-rules/:id', pricingRulesController.deletePricingRule);
+router.get("/pricing-rules/:id", pricingRulesController.getPricingRuleById);
+router.put("/pricing-rules/:id", pricingRulesController.updatePricingRule);
+router.delete("/pricing-rules/:id", pricingRulesController.deletePricingRule);
 
 router.get("/service-category", controller.listServiceCategories);
 router.post("/service-category", controller.createServiceCategories);
@@ -31,4 +38,9 @@ router.get("/service-bay", controllerServiceBays.listServiceBays);
 router.post("/service-bay", controllerServiceBays.createServiceBay);
 router.put("/service-bay/:id", controllerServiceBays.updateServiceBay);
 router.delete("/service-bay/:id", controllerServiceBays.removeServiceBay);
+
+router.get("/warranty-policies", warrantyController.getWarrantyPolicies);
+router.post("/warranty-policies", warrantyController.createWarrantyPolicy);
+router.put("/warranty-policies/:id", warrantyController.updateWarrantyPolicy);
+
 module.exports = router;
