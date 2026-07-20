ba7ea01a85d6fab02db9d791ac2cc50b063f3be5
Manh <manhselu@gmail.com>
2026-07-20 22:52:55 +0700

    feat(service-quality-inspection-management): implement get task pending qc, approve, reject task pending qc

--- src/router/technicianLeader/technicianLeader.routes.js
+++ src/router/technicianLeader/technicianLeader.routes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const serviceQualityInspectionController = require("../../controller/technicianLeader/serviceQualityInspection.controller");
 
 router.get("/quality-inspection", serviceQualityInspectionController.getTasksPendingQC);
+router.patch("/final-inspection/:serviceOrderId/approve", serviceQualityInspectionController.approveFinalInspection);
+router.patch("/final-inspection/:serviceOrderId/reject", serviceQualityInspectionController.rejectFinalInspection);
 router.get("/tasks", taskAssignmentController.getAllTasks);
 router.get("/technicians", taskAssignmentController.getAllTechnician);
 router.post("/assign", taskAssignmentController.assignTask);
